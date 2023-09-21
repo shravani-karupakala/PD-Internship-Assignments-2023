27d76960da61c87207f24ad0ca815a49423b0782
k-shravani18 <karupakalashravani@gmail.com>
2023-09-21 16:26:54 +0530

    access token

--- script.js
+++ script.js
@@ -1,60 +1,94 @@
-const token = 'BQBr6MCuSUj1ih0yV4BJ8D20hRU6gNcZ-qysxqWOHkPxxoifnKne_l_J3fz_3pEAuUMIH3OR2CBX8t5_lhSOZs_ycchH_D7N0JRs8j0Q8s5e3EBO2iqMyIrBpWguM195klji-4bj3HPC2BMiPdzEqF9AOl2HKzbYhOg0gqULEuQHBvNb6_-uyH__g1f0O5sRuctlOSDnUxiqB1MvTS-GMpFLa5UmWbXucXkKTG6VRDZzeo4iNZuyhVr283eG4aU5Rk5BYdojz6pY3XeDi-Is6c6f';
-const artistIds =    ['5VVN3xZw1i2qihfITZlvCZ','4A2XSc4OJjuPY4l6NjnrDj','5sSzCxHtgL82pYDvx2QyEU','4W91bbPB2CTSsHwt7eqNl7','2nWYQRy7Ikh7CyWnvZZouD',
-'0wv5i0ds2z040yx7oL6UZy','2AQwMDyDvgpZcHLITOpgpm','5FVBduYaeVBb6JIghza7v6','2bfyLSCw72lQ5qoD8cdVBB','2IUtwMti1OiT3lkW6RubgH'
-];
 
+const displayArtistDetails = (function () {
+  const clientId = 'd2289543fab74ad292812cbd64d26008'
+  const clientSecret = 'd45db6fd8f3240fd89e54a5baf623e7f';
 
+  let token; 
 
-async function fetchArtistDetails(artistId) {
-    // for (const artistId of artistIds) {
-      const url = `https://api.spotify.com/v1/artists/${artistId}`;
-  
-      const request = new Request(url, {
+  // Private method to get the access token
+  const getAccessToken = async () => {
+    try {
+      const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
         headers: {
-          'Authorization': `Bearer ${token}`
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
         },
+        body: 'grant_type=client_credentials'
       });
-  
-      const response = await fetch(request);
-  
-      if (response.status === 200) {
-        // const data = await response.text();
-        // document.getElementById("artist-details").innerHTML=data
-        // console.log(`Data for Artist ID ${artistId}:`, data);
-        const data = await response.json();
-        return data;
-      } else {
-        console.error(`Error for Artist ID ${artistId}: ${response.status} - ${response.statusText}`);
-        return null;
-      }
-    // }
-  }
-  
-  
-  async function displayArtistDetails() {
-    const artistDetailsContainer = document.getElementById("artist-details");
 
-    for (const artistId of artistIds) {
-        const artistData = await fetchArtistDetails(artistId);
-
-        if (artistData) {
-            const artistName = artistData.name;
-            const artistImage = artistData.images[0].url; // Assuming you want the first image
-            const artistGenres = artistData.genres.join(', ');
-            const spotifyLink = artistData.external_urls.spotify;
-
-            const artistElement = document.createElement("div");
-            artistElement.innerHTML = `
-                <h2>${artistName}</h2>
-                <img src="${artistImage}" alt="${artistName}" width="200">
-                <p>Genres: ${artistGenres}</p>
-                <p><a href="${spotifyLink}" target="_blank">Spotify Profile</a></p>
-                <hr>
-            `;
-
-            artistDetailsContainer.appendChild(artistElement);
-        }
+      const data = await result.json();
+      token = data.access_token;
+    } catch (error) {
+      console.error("Error fetching token:", error);
     }
-}
+  }
 
-displayArtistDetails();
+  // Public method to fetch data from Spotify API
+  const fetchArtistDetails = async () => {
+    if (!token) {
+      await getAccessToken();
+    }
+
+    const url = 'https://api.spotify.com/v1/artists?ids=5VVN3xZw1i2qihfITZlvCZ,4A2XSc4OJjuPY4l6NjnrDj,5sSzCxHtgL82pYDvx2QyEU,4W91bbPB2CTSsHwt7eqNl7,2nWYQRy7Ikh7CyWnvZZouD,0wv5i0ds2z040yx7oL6UZy,2AQwMDyDvgpZcHLITOpgpm,5FVBduYaeVBb6JIghza7v6,2bfyLSCw72lQ5qoD8cdVBB,2IUtwMti1OiT3lkW6RubgH'
+
+    const request = new Request(
+      url, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+    });
+
+    try {
+      const response = await fetch(request);
+      const data = await response.json();
+
+      const dataContainer = document.getElementById("artist-details");
+
+      data.artists.forEach(artist => {
+        const artistContainer = document.createElement("div");
+        artistContainer.classList.add("artist-container");
+
+        if (artist.images.length > 0) {
+          const artistImage = document.createElement("img");
+          artistImage.src = artist.images[0].url;
+          artistImage.alt = artist.name;
+          artistContainer.appendChild(artistImage);
+        }
+
+        const artistDetails = document.createElement("div");
+        artistDetails.classList.add("artist-details");
+
+        const artistName = document.createElement("h2");
+        artistName.textContent = artist.name;
+
+        const artistGenres = document.createElement("p");
+        artistGenres.textContent = `Genres: ${artist.genres.join(', ')}`;
+
+        const spotifyLink = document.createElement("p");
+        const spotifyAnchor = document.createElement("a");
+        spotifyAnchor.href = artist.external_urls.spotify;
+        spotifyAnchor.target = "_blank";
+        spotifyAnchor.textContent = "Spotify Profile";
+
+        spotifyLink.appendChild(spotifyAnchor);
+        artistDetails.appendChild(artistName);
+        artistDetails.appendChild(artistGenres);
+        artistDetails.appendChild(spotifyLink);
+
+        artistContainer.appendChild(artistDetails);
+
+        dataContainer.appendChild(artistContainer);
+      });
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  return {
+    fetchArtistDetails: fetchArtistDetails
+  };
+})();
+
+displayArtistDetails.fetchArtistDetails();
